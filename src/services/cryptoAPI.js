c17278c2cabf5bb16f9a793789155a4efe70701e
Dylan Dalton <dylandalton.za@gmail.com>
2024-11-20 20:08:48 +0000

    FEATURE: Setup redux store, Created RapidAPI crypto service

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    
+    baseQuery: fetchBaseQuery({
+        baseUrl,
+        prepareHeaders: (headers) => {
+            headers.set('x-rapidapi-key', '7fed9efa2fmshd58334dc47ec098p1f6e6ajsnc3a70bc9e35d');
+            headers.set('x-rapidapi-host', 'coinranking1.p.rapidapi.com');
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => '/exchanges',
+        })
+    })
+});
